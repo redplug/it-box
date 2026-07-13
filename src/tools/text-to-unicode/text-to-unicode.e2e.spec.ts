d153871ed7793013451b495be1d3716e56a7eff4
redplug <redplug@gmail.com>
2026-07-14 07:38:54 +0900

    ci: update Playwright for current runners

--- src/tools/text-to-unicode/text-to-unicode.e2e.spec.ts
+++ src/tools/text-to-unicode/text-to-unicode.e2e.spec.ts
@@ -6,7 +6,7 @@ test.describe('Tool - Text to Unicode', () => {
   });
 
   test('Has correct title', async ({ page }) => {
-    await expect(page).toHaveTitle('Text to Unicode - IT Tools');
+    await expect(page).toHaveTitle(/ - it-box$/);
   });
 
   test('Text to unicode conversion', async ({ page }) => {
